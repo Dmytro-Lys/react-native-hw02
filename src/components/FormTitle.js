adf995b90da05b85662fbe370a4f805325a0391e
Dmytro-Lys <d.lysenko122@gmail.com>
2023-10-23 16:07:49 +0300

    add components for form

--- src/components/FormTitle.js
+++ src/components/FormTitle.js
@@ -1,7 +1,16 @@
 import { Text } from "react-native";
 
-const FormTitle = ({ text }) => {
-    <Text style={{ fontFamily: 'Roboto-Medium', fontSize: 30, letterSpacing: 0.3, textAlign: 'center' } }>{text}</Text>
+const FormTitle = ({ text, handleMarginTop = 32  }) => {
+    return (
+    <Text style={{
+        fontFamily: 'Roboto-Medium',
+        fontSize: 30,
+        letterSpacing: 0.3,
+        textAlign: 'center',
+        marginTop: handleMarginTop,
+        marginBottom: 32,
+        }}>{text}</Text>
+    )
 }
 
 export default FormTitle;
--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -1,33 +1,78 @@
 import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+import { useState } from "react";
+// import { nanoid } from 'nanoid';
 import bgImage from '../../assets/images/photo_bg.jpg'
 import defaultAvatar from '../../assets/images/avatar1.jpg'
-import { Avatar } from '../components';
+import { Avatar, FormTitle, Input, FormSubmitButton, LinkButton } from '../components';
+
 
 
 const styles = StyleSheet.create({
    imageBg: {
     flex: 1,
+    // flexDirection: 'column',
     justifyContent: 'center',
+
   },
   form: {
     position: 'relative',
     flex: 1,
     marginTop: 263,
-    flexDirection: "column",
-    justifyContent: 'center',
+    padding: 16,
+    // flexDirection: "column",
+    // maxHeight: '100vh',
+    // maxWidth: '100vw',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
     backgroundColor: '#fff',
 
-  }
+  },
+  formElements: {
+    flex: 1,
+    // maxHeight: 182,
+    // flexDirection: "column",
+     rowGap: 16,
+  },
+ 
 });
 
 const RegistrationScreen = () => {
+  
+ 
+   const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    alert("Submit")
+  }
+
+  const reset = () => {
+    setUserName("")
+    setEmail("")
+    setPassword("")
+  }
+
+  const onLink = () => {
+    alert("Link")
+  }
+  
   return (
       <ImageBackground source={bgImage} resizeMode="cover" style={styles.imageBg}>
         <View style={styles.form}>
           {/* <Avatar avatarImage={defaultAvatar}/> */}
-          <Avatar/> 
+        <Avatar /> 
+      
+        <FormTitle text="Реєстрація" handleMarginTop={92} />
+          <View style={styles.formElements}>
+            <Input inputName="userName" handleChange= {setUserName} inputValue={userName} />
+            <Input inputName="email" handleChange= {setEmail} inputValue={email}/>
+            <Input inputName="password" handleChange= {setPassword} inputValue={password}/>
+          </View>
+          <FormSubmitButton title="Зареєструватися" onPress={onSubmit} />
+          <LinkButton title="Вже є акаунт? Увійти" onPress={onLink}/>
         </View> 
       </ImageBackground>   
   );
